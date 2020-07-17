96db9bd57cd00738c063aac3d3c2b1ddeed27801
brunohenriqd <60947770+brunohenriqd@users.noreply.github.com>
2020-07-16 23:21:57 -0300

    Add files via upload

---
+++ App_BD_SQLite/App_BD_SQLite/View/CadastrarDetail.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using App_BD_SQLite.Model;
+using App_BD_SQLite.ViewModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_BD_SQLite.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CadastrarDetail : ContentPage
+    {
+        int id;
+        public CadastrarDetail()
+        {
+            InitializeComponent();
+        }
+
+        public CadastrarDetail(ModelNota nota) 
+        {
+            InitializeComponent();
+            btSalvar.Text = "Alterar";
+
+            btExcluir.IsVisible = true;
+
+            id = nota.Id;
+            txtTitulo.Text = nota.Titulo;
+            txtDados.Text = nota.Dados;
+            swFavorito.IsToggled = nota.Favorito;
+        }
+
+        private void btSalvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelNota nota = new ModelNota();
+                nota.Titulo = txtTitulo.Text;
+                nota.Dados = txtDados.Text;
+                nota.Favorito = swFavorito.IsToggled;
+                ServicesBDNota dbNotas = new ServicesBDNota(App.DbPath);
+                if (btSalvar.Text == "Inserir")
+                {
+                    dbNotas.Inserir(nota);
+                    DisplayAlert("Resultado da operação", dbNotas.StatusMessage, "OK");
+                }
+                else
+                { //ALTERAR EM PROXIMA AULA
+                    nota.Id = id;
+                    dbNotas.Alterar(nota);
+                    DisplayAlert("Resultado da operação", dbNotas.StatusMessage, "OK");
+                }
+                voltar();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+        public void voltar()
+        {
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new HomeDetail());
+        }
+
+        private void btCancelar_Clicked(object sender, EventArgs e)
+        {
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new HomeDetail());
+        }
+
+        private async void btExcluir_Clicked(object sender, EventArgs e)
+        {
+            bool resp = await DisplayAlert("Excluir Registro",
+                "Deseja excluir a nota atual?",
+                "sim", "não");
+
+            if (resp)
+            {
+                ServicesBDNota dbNotas = new ServicesBDNota(App.DbPath);
+                dbNotas.Excluir(id);
+                await DisplayAlert("Resultado da operação", dbNotas.StatusMessage, "OK");
+            }
+            voltar();
+        }
+    }
+}
---
+++ App_BD_SQLite/App_BD_SQLite/View/ListarDetail.xaml.cs
@@ -0,0 +1,37 @@
+﻿using App_BD_SQLite.Model;
+using App_BD_SQLite.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_BD_SQLite.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListarDetail : ContentPage
+    {
+        public ListarDetail()
+        {
+            InitializeComponent();
+            AtualizaLista();
+        }
+
+        public void AtualizaLista()
+        {
+            ServicesBDNota dbNotas = new ServicesBDNota(App.DbPath);
+            ListaNotas.ItemsSource = dbNotas.Listar();
+        }
+
+        private void ListaNotas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ModelNota nota = (ModelNota)ListaNotas.SelectedItem;
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new CadastrarDetail(nota));
+        }
+    }
+}
